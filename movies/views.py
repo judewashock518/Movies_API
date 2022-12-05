e6c9179d9866948ea742af87cb71862f192588fd
JW <114599894+judewashock518@users.noreply.github.com>
2022-12-05 01:11:07 -0500

    registered movie model with admin site and completed post functionality for movie and updated rest framework decimal settings

--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Movie
+from .serializers import MovieSerializer
+from rest_framework import status
+
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movies_list(request):
 
-    return Response('ok')
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
